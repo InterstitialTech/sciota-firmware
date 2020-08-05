fd43ff53523b6743c13a7b29426c6c268e54731f
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2020-08-04 23:50:33 -0700

    serial.c: switch to USART3 (PC10/PC11)

--- src/serial.c
+++ src/serial.c
@@ -11,19 +11,22 @@ extern int _write(int, const char *, ssize_t);
 
 void serial_setup(void) {
 
-    rcc_periph_clock_enable(RCC_USART1);
+    // USART3 on PC10 (TX) and PC11 (RX)
 
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
-    gpio_set_af(GPIOA, GPIO_AF7, GPIO9);
+    rcc_periph_clock_enable(RCC_USART3);
+    rcc_periph_clock_enable(RCC_GPIOC);
 
-    usart_set_baudrate(USART1, 115200);
-    usart_set_databits(USART1, 8);
-    usart_set_parity(USART1, USART_PARITY_NONE);
-    usart_set_stopbits(USART1, USART_CR2_STOPBITS_1);
-    usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-    usart_set_mode(USART1, USART_MODE_TX);  // just output for now
+    gpio_mode_setup(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10 | GPIO11);
+    gpio_set_af(GPIOC, GPIO_AF7, GPIO10 | GPIO11);
 
-    usart_enable(USART1);
+    usart_set_baudrate(USART3, 115200);
+    usart_set_databits(USART3, 8);
+    usart_set_parity(USART3, USART_PARITY_NONE);
+    usart_set_stopbits(USART3, USART_CR2_STOPBITS_1);
+    usart_set_flow_control(USART3, USART_FLOWCONTROL_NONE);
+    usart_set_mode(USART3, USART_MODE_TX_RX);  // duplex
+
+    usart_enable(USART3);
 
 }
 
@@ -38,13 +41,10 @@ int _write(int file, const char *ptr, ssize_t len) {
 
     ssize_t i;
     for (i = 0; i < len; i++) {
-
         if (ptr[i] == '\n') {
-            usart_send_blocking(USART1, '\r');
+            usart_send_blocking(USART3, '\r');
         }
-
-        usart_send_blocking(USART1, ptr[i]);
-
+        usart_send_blocking(USART3, ptr[i]);
     }
 
     return i;
